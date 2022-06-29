a2ccf04c8515c9370796097347fb30566bc24533
james_siao <james_siao@outlook.com>
2022-06-29 09:57:12 +0800

    修改部分注释

--- bulk_dir_rename.m
+++ bulk_dir_rename.m
@@ -1,5 +1,5 @@
-% 批量对文件夹改名
-% 如文件加名为testA，改为testB。具体文件夹名为：1_2月 - 附件改为2_2月
+% 批量对文件夹改名，中文名称文件夹也没问题。
+% 如文件加名为testA，改为testB。具体文件夹名为：'1_2月 - 附件'改为'2_2月'
 % 2022-6-28  测试成功。
 selpath = uigetdir;   % 交互对话框，指定文件夹
 ADir = dir(selpath);
@@ -7,7 +7,7 @@ ADir = ADir([ADir.isdir]);
 AName = {ADir.name};
 
 AName(strncmp(AName, '.', 1)) = [];  % strncmp(s1,s2,n),比较字符串s1、s2前n个字符是否相等，相等返回1.
-                                     % 本行语句是与.对比，就是除去1个点和2个点的文件夹
+                                     % 本行语句是与.对比，以从列表中剔除名为1个点和2个点的文件夹
 for iFolder = 1:numel(AName)
     if ~isempty(strfind(AName{iFolder},'副本'))
         newname = erase(AName{iFolder},' - 副本');   % 删除' - 副本'
